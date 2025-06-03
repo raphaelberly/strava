685bcebaeae61b7e79b3c3085db632b65631b2ec
Raphaël Berly <raphaelberly@gmail.com>
2025-06-03 10:52:34 +0200

    Add duplicate activity checker

---
+++ check_activities.py
@@ -0,0 +1,39 @@
+import yaml
+
+from lib.database import Database
+from lib.push import Push
+
+# Read configuration files
+secrets = yaml.safe_load(open('conf/secrets.yaml'))
+
+# Configure notification service
+push = Push(**secrets['push'])
+
+# Configure database connector
+db = Database(**secrets['db'])
+
+query = """
+WITH
+activity_lags AS (
+    SELECT
+        a.name,
+        lag(a.name) OVER (ORDER BY a.start_datetime_utc) AS lag_name,
+        a.type,
+        lag(a.type) OVER (ORDER BY a.start_datetime_utc) AS lag_type,
+        date_trunc('hour', a.start_datetime_utc) AS date,
+        lag(date_trunc('hour', a.start_datetime_utc)) OVER (ORDER BY a.start_datetime_utc) AS lag_date
+    FROM strava.activities a
+)
+SELECT name, lag_name
+FROM activity_lags a
+WHERE a.type = a.lag_type
+    AND a.date = a.lag_date
+    AND date(date) >= date(current_date) - INTERVAL '1' MONTH
+"""
+results = db.run_query(query)
+if len(results) > 0:
+    for item in results.to_dict(orient="records"):
+        push.send_message(
+            message=f'''Similar activities found: "{item['name']}" and "{item['lag_name']}"''',
+            title="⚠️ Potential Strava duplicates"
+        )
